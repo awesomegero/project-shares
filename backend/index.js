0ca031bb2098e79e73e0726a829385a242495827
awesomegero <gero.lindner@protonmail.com>
2021-10-18 14:07:40 +0200

    Added mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const crypto = require("crypto");
 dotenv.config();
 const jwtSecret = crypto.randomBytes(64);
 const port = process.env.API_PORT || 3000;
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017";
 const app = express();
+
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log(`Project Shares backend database connected`);
+    });
 
 app.listen(port, () => {
     console.log(`Project Shares backend server listening on ${port}`);
